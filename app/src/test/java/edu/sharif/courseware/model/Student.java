357a813eb0c734b282d10cba69aac50cc2fda689
Sadegh <mohammadsadeghmajidiyazdi@hotmail.com>
2022-03-10 17:50:40 +0330

    Create Student class

---
+++ app/src/test/java/edu/sharif/courseware/model/Student.java
@@ -0,0 +1,18 @@
+package edu.sharif.courseware.model;
+
+public class Student extends User {
+    private String studentNumber;
+
+    public Student(String username, String password, String firstname, String lastname, String studentNumber) {
+        super(username, password, firstname, lastname);
+        this.studentNumber = studentNumber;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+}
