4959e902b848eb43c51be705bc691ed75d4ffe6e
Arya Jalali <nothatcreative5@gmail.com>
2022-03-13 17:09:43 +0330

    Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/edu/sharif/courseware/ProfessorCreateClass.java
@@ -0,0 +1,14 @@
+package edu.sharif.courseware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessorCreateClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_create_class);
+    }
+}
---
+++ app/src/main/java/edu/sharif/courseware/ProfessorMainPage.java
@@ -0,0 +1,14 @@
+package edu.sharif.courseware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessorPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_main_page);
+    }
+}
